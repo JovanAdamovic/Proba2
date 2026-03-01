02c42098c0e3b4cc60010e6cc6bcad0dbfcc56c8
JovanAdamovic <jadamovic538@gmail.com>
2026-03-01 19:51:19 +0100

    20. docker radi

--- evidencije_react/src/api/http.js
+++ evidencije_react/src/api/http.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8080/api";
+
 const http = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: API_BASE_URL,
 });
 
 http.interceptors.request.use((config) => {
