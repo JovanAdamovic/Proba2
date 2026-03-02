877468eb4c70692a6f50f9964ced85e6cc0a82e0
JovanAdamovic <jadamovic538@gmail.com>
2026-03-02 14:52:23 +0100

    21. Swagger

--- evidencije_laravel/app/Http/Controllers/Controller.php
+++ evidencije_laravel/app/Http/Controllers/Controller.php
@@ -1,6 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+
+#[OA\Info(
+    version:"1.0.0",
+    title:"Evidencije API",
+    description:"API za upravljanje predmetima, zadacima, predajama, upisima, kalendarom rokova i proverama plagijata."
+)]
+ 
+
 
 abstract class Controller
 {
